eb5041f3bac8ebc470c7b17cb126f5b5cc3f4c05
devaanshG <devaanshnz@outlook.com>
2024-11-28 19:55:52 +0530

    Added config options to auth.ts

--- src/auth.ts
+++ src/auth.ts
@@ -5,5 +5,14 @@ import { pool } from "@/lib/db";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PostgresAdapter(pool),
-  providers: [Google],
+  providers: [
+    Google({
+      authorization: {
+        params: {
+          redirect_uri: process.env.NEXTAUTH_URL,
+        },
+      },
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
 });
